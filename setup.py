cfe98bf5cb0b174b2b5c95faf00f707b048f6d51
moreal <dev.moreal@gmail.com>
2018-09-29 23:47:56 +0900

    Add setup.py to prepare publish

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='dms',
+    description='An DSM-DMS BackEnd Python Wrapper',
+    version='0.0.1',
+    url='https://github.com/moreal/dms-api',
+    author='Moreal'
+    author_email='dev.moreal@gmail.com',
+    license='MIT',
+    install_requires=[
+        'requests>=2.19.1'
+    ]
+)
